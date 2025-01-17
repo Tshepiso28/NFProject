0e3c9db7df3a7a4c96ce768055438bec7aeed58c
Tshepiso28 <tshepisostudent@gmail.com>
2025-01-17 15:55:31 +0200

    Redesigned the home page

--- JavaScript/notFacebook.js
+++ JavaScript/notFacebook.js
@@ -32,12 +32,7 @@ postButton.addEventListener('click', () => {
         return;
     }
 
-    const label = document.createElement('label');
-    label.setAttribute('for', 'username');
-    label.textContent = `${loggedInUser}`;
 
-    const userProfileDiv = document.querySelector('.user-profile');
-    userProfileDiv.appendChild(label);
 
 
     const postDiv = document.createElement('div');
